778247d06e910972ad1dc81864912345789d4da0
limaxrayxray <79624262+limaxrayxray@users.noreply.github.com>
2024-06-17 11:13:11 -0400

    Update TaskList.js

--- components/Task/TaskList.js
+++ components/Task/TaskList.js
@@ -1,1 +1,12 @@
+// components/Task/TaskList.js
+import TaskItem from './TaskItem';
 
+const TaskList = ({ tasks }) => (
+  <div>
+    {tasks.map(task => (
+      <TaskItem key={task.id} task={task} />
+    ))}
+  </div>
+);
+
+export default TaskList;
