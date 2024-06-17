2171008fdd4440038d2d7990b29f5f6d4ec304e3
limaxrayxray <79624262+limaxrayxray@users.noreply.github.com>
2024-06-17 11:12:51 -0400

    Update Header.js

--- Layout/Header.js
+++ Layout/Header.js
@@ -1,1 +1,14 @@
+// components/Layout/Header.js
+import Link from 'next/link';
 
+const Header = () => (
+  <header className="bg-blue-500 p-4 text-white flex justify-between items-center">
+    <h1 className="text-xl">To-Do List App</h1>
+    <nav>
+      <Link href="/"><a className="mr-4">Home</a></Link>
+      <Link href="/settings"><a>Settings</a></Link>
+    </nav>
+  </header>
+);
+
+export default Header;
